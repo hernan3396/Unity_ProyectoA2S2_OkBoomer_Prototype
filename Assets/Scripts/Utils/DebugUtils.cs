25095d1ca2c053906082fab5a6d7e56dd906a642
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-07-25 21:25:08 -0300

    agregada death zone

--- Assets/Scripts/Utils/DebugUtils.cs
+++ Assets/Scripts/Utils/DebugUtils.cs
@@ -3,7 +3,8 @@ using TMPro;
 
 public class DebugUtils : MonoBehaviour
 {
-    [SerializeField] private PlayerScriptable _player;
+    [SerializeField] private Transform _playerPos;
+    [SerializeField] private PlayerScriptable _playerData;
     [SerializeField] private TMP_Text _speedText;
     [SerializeField] private TMP_Text _jumpStrengthText;
     [SerializeField] private TMP_Text _gravityText;
@@ -13,48 +14,53 @@ public class DebugUtils : MonoBehaviour
 
     private void Start()
     {
-        _speedText.text = _player.Speed.ToString();
-        _jumpStrengthText.text = _player.JumpStrength.ToString();
-        _gravityText.text = _player.Gravity.ToString();
-        _sensitivityText.text = _player.MouseSensitivity.ToString();
-        _coyoteText.text = _player.CoyoteMaxTime.ToString();
-        _apexText.text = _player.HalfGravityLimit.ToString();
+        _speedText.text = _playerData.Speed.ToString();
+        _jumpStrengthText.text = _playerData.JumpStrength.ToString();
+        _gravityText.text = _playerData.Gravity.ToString();
+        _sensitivityText.text = _playerData.MouseSensitivity.ToString();
+        _coyoteText.text = _playerData.CoyoteMaxTime.ToString();
+        _apexText.text = _playerData.HalfGravityLimit.ToString();
     }
 
     public void ChangeSpeed(int value)
     {
-        _player.Speed += value;
-        _speedText.text = _player.Speed.ToString();
+        _playerData.Speed += value;
+        _speedText.text = _playerData.Speed.ToString();
     }
 
     public void ChangeJump(int value)
     {
-        _player.JumpStrength += value;
-        _jumpStrengthText.text = _player.JumpStrength.ToString();
+        _playerData.JumpStrength += value;
+        _jumpStrengthText.text = _playerData.JumpStrength.ToString();
     }
 
     public void ChangeGravity(int value)
     {
-        _player.Gravity += value;
-        _gravityText.text = _player.Gravity.ToString();
+        _playerData.Gravity += value;
+        _gravityText.text = _playerData.Gravity.ToString();
     }
 
     public void ChangeSensitivity(int value)
     {
-        if (_player.MouseSensitivity <= 1 && value < 0) return; // para que no quede en 0 jaj
-        _player.MouseSensitivity += value;
-        _sensitivityText.text = _player.MouseSensitivity.ToString();
+        if (_playerData.MouseSensitivity <= 1 && value < 0) return; // para que no quede en 0 jaj
+        _playerData.MouseSensitivity += value;
+        _sensitivityText.text = _playerData.MouseSensitivity.ToString();
     }
 
     public void ChangeCoyote(float value)
     {
-        _player.CoyoteMaxTime += value;
-        _coyoteText.text = _player.CoyoteMaxTime.ToString();
+        _playerData.CoyoteMaxTime += value;
+        _coyoteText.text = _playerData.CoyoteMaxTime.ToString();
     }
 
     public void ChangeApexTime(float value)
     {
-        _player.HalfGravityLimit += value;
-        _apexText.text = _player.HalfGravityLimit.ToString();
+        _playerData.HalfGravityLimit += value;
+        _apexText.text = _playerData.HalfGravityLimit.ToString();
+    }
+
+    public void MovePlayer(Transform endPos)
+    {
+        _playerPos.position = endPos.position;
     }
 }
